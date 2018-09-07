3db95cb373c6ffa77923afdbe9c3f30cb390a214
Manabu Niseki <zgok0079@gmail.com>
2018-09-08 06:55:33 +0900

    feat: add discover command (#3)

--- lib/ryo/error.rb
+++ lib/ryo/error.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 module Ryo
   class NotFoundError < StandardError; end
+  class InvalidURLError < StandardError; end
+  class InvalidOptionsError < StandardError; end
 end
