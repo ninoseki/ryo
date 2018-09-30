154331e3bfa48bdb817e2f4f8b4d663962eee6ac
Manabu Niseki <manabu.niseki@gmail.com>
2018-09-30 21:06:18 +0900

    test: change to follow GitLab's testing best practices

--- spec/plugin/tech_spec.rb
+++ spec/plugin/tech_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe Ryo::Plugin::Tech, :vcr do
   subject { Ryo::Plugin::Tech }
-  describe "#discover" do
+  describe ".discover" do
     it "should return a Hash" do
       results = subject.discover "https://www.webscantest.com/"
       expect(results).to be_an(Hash)
--- spec/plugin/whois_spec.rb
+++ spec/plugin/whois_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe Ryo::Plugin::Whois, :vcr do
   subject { Ryo::Plugin::Whois }
-  describe "#discover" do
+  describe ".discover" do
     it "should return a Hash" do
       results = subject.discover "example.com"
       expect(results).to be_an(Hash)
